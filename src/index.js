365de826d02166c5d30aa6679c44925f3520f170
Anish Gadekar <argads@gmail.com>
2020-08-09 18:37:25 -0700

    Add roboto font.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
+import "typeface-roboto";
 
 ReactDOM.render(
   <React.StrictMode>
